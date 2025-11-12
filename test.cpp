c1b26f29fc330ee6d24becb480aedfd304891482
noahbaldinger <baldingernoah@proton.me>
2025-11-12 11:16:26 +0100

    testing file and added "using namespace std;" instead of "std::" infront of most vars

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+  string test = "test";
+  cout << test << std::endl;
+  return 0;
+}
